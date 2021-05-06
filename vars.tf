baa9825601b07fcecbb0d1e3bb722c6858d6227d
Damodaran Rajamanickam <damodaranrajamanickam@192.168.0.104>
2021-05-06 20:56:07 +0530

    sg-vars fixed

--- vars.tf
+++ vars.tf
@@ -3,6 +3,8 @@ variable "ami_id" { default = "ami-096fda3c22c1c990a"}
 variable "instance_type" { default = "t2.micro"}
 variable "key_name" {default = "jenkins-sg"} 
 variable "security_groups" {
-    type = "list"
-    default = ["sg-07f1194be45d29d97"]}
+    type = list(string)
+    default = ["sg-07f1194be45d29d97"]
+    }
+
 variable "subnet_id" {default = "subnet-028806318df3b368f"}
